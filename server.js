9057762518b57b47536a479e31c2ebfa4c42ec04
Tom LaChance <guitarlanguageseries@gmail.com>
2018-04-15 18:10:08 -0500

    added process.env to port in server. line 14

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+ar port = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
